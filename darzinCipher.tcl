b8437ab729d2f08723c085b4592bf84b1ccbc3f4
uniquedm <47504086+uniquedm@users.noreply.github.com>
2019-03-19 09:05:55 +0530

    Add files via upload

--- darzinCipher.tcl
+++ darzinCipher.tcl
@@ -1,19 +1,17 @@
-set pt "ABCEDF"
+puts "Enter your plain text(string):"
+gets stdin pt
 set pt [string toupper $pt]
-set key 123
-
+puts "Enter your key(int):"
+gets stdin key
 set klen [string length $key]
 for {set i 0} {$i<[string length $pt]} {incr i 3} {
     for {set j 0} {$j<$klen} {incr j} {
         append l($i) [string index $pt [expr $j+$i]]
     }
-    puts "Test 1: $l($i) $i"
 }
-
 for {set i 0} {$i<[string length $key]} {incr i} {
     set kl($i) [expr [string index $key $i]]
 }
-
 for {set i 0} {$i<=[array size l]+1} {incr i 3} {
     for {set j 0} {$j<[string length $l($i)]} {incr j} {
         set ln [scan [string index $l($i) $j] %c]
